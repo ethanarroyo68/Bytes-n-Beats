1935ffffc5b225226595507c7084137487ef4a5a
ethanarroyo68 <ethanarroyo68@gmail.com>
2025-04-14 15:11:58 -0600

    nothing

--- key_analysis.py
+++ key_analysis.py
@@ -4,10 +4,10 @@ from musical_instrument import MusicalInstrument
 # Load the MIDI file
 midi_data = pretty_midi.PrettyMIDI("/s/bach/l/under/c835266433/Bytes-n-Beats/midi_solo.mid")
 
-guitar_solo = MusicalInstrument(midi_data.instruments[0])
+"""guitar_solo = MusicalInstrument(midi_data.instruments[0])
 guitar_solo.print_notation()
 print()
 print("How many times each note occurs:")
 guitar_solo.print_note_count()
-
+"""
 
